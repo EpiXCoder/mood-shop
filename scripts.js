10c1a09a878535169bd81c36aca922f6292a4e6f
Vithusha Ravirajan <vithusharavirajan@Vithushas-MacBook-Pro.local>
2022-09-28 21:51:12 -0400

    added removeItem function

--- scripts.js
+++ scripts.js
@@ -54,12 +54,11 @@ for (let i = 0; i < data.length; i += 1) {
 	itemsContainer.appendChild(newDiv)
 }
 
+//------------------------------------------------
 const cart = [ ]
+const obj = {}
 
-const obj = {
-
-}
-
+// Add Items
 function addItem(name, price) {
     for (let i = 0; i <cart.length; i++){
         if (cart[i].name === name) {
@@ -67,38 +66,70 @@ function addItem(name, price) {
             return
         }
     }
-    const item = {name: name, price: price, qty: 1}
+    const item = {name, price, qty: 1}
     cart.push(item)
 }
 
+//------------------------------------------------
+// Show Items
 function showItems() {
     console.log(`you have ${getQty()} items in your cart`)
+
     for (let i = 0; i <cart.length; i +=1) {
-        console.log(`- ${cart[i].name} ${cart[i].price} x ${cart[i]}`)
+        console.log(`- ${cart[i].name} ${cart[i].price} x ${cart[i].qty}`)
     }
 
-
-    console.log(`Total in cart: $${getTotal().toFixed(2)}`)
+    console.log(`Total in cart: $${getTotal()}`)
 }
-
+//------------------------------------------------
+// Get Qty
 function getQty() {
     let qty = 0;
-    for (let i = 0; i <cart.length; i +=1) {
+    for (let i = 0; i < cart.length; i +=1) {
         qty += cart[i].qty
     }
     // console.log(`you have ${qty} items in your cart`)
     return qty;
 }
 
+//------------------------------------------------
+// Get total
 function getTotal() {
     let total = 0;
     for (let i = 0; i <cart.length; i +=1) {
-        total += cart[i].price * cart[i].qty;
+        total += cart[i].price * cart[i].qty
+    }
+    return total.toFixed(2)
+}
+//------------------------------------------------
+// Remove Items
+
+function removeItem(name, qty = 0) {
+    for (let i = 0; i < cart.length; i++) {
+        if (cart[i].name === name) {
+            if (qty > 0){
+            cart[i].qty -= qty
+            }
+            if (cart[i].qty < 1 || qty === 0) {
+                cart.splice(i, 1)
+            }
+            
+            return
+        }
     }
 }
 
+
+
+//------------------------------------------------
 addItem('Apple', 0.99)
 addItem('Apple', 0.99)
 addItem('Opinion', 0.02)
 addItem('Frisbee', 9.92)
+addItem('Apple', 0.99)
+addItem('Orange', 1.99)
+addItem('Jacket', 50.00)
+showItems()
+removeItem('Apple', 1)
+removeItem('Frisbee')
 showItems()
